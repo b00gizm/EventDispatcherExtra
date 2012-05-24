da76b54599678b53f1656eedf5275b10679e10b3
b00giZm <b00gizm@gmail.com>
2012-05-24 15:48:58 +0200

    Added unit tests

---
+++ test/CodeNugget/EventDispatcherExtra/Amqp/AmqpAwareEventDispatcherTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Test\CodeNugget\EventDispatcherExtra\Amqp;
+
+use CodeNugget\EventDispatcherExtra\Amqp\QueueEvent,
+    CodeNugget\EventDispatcherExtra\Amqp\AmqpAwareEventDispatcher;
+
+class AmqpAwareEventDispatcherTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->conn = $this->getMockBuilder('PhpAmqpLib\Connection\AMQPConnection')
+                           ->disableOriginalConstructor()
+                           ->getMock();
+
+        $this->dispatcher = new AmqpAwareEventDispatcher($this->conn);
+    }
+
+    public function tearDown()
+    {
+        unset($this->dispatcher);
+        unset($this->conn);
+    }
+
+    public function testDispatch()
+    {
+        $channel = $this->getMockBuilder('PhpAmqpLib\Channel\AMQPChannel')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+
+        $channel->expects($this->once())
+                ->method('basic_publish')
+                ->with(
+                    $this->isInstanceOf('PhpAmqpLib\Message\AMQPMessage'),
+                    $this->equalTo(null),
+                    $this->equalTo('')
+                );
+
+        $channel->expects($this->once())
+                ->method('close');
+
+        $this->conn->expects($this->once())
+                   ->method('channel')
+                   ->will($this->returnValue($channel));
+
+        $this->dispatcher->dispatch('my.test.event', new QueueEvent());
+    }
+
+    public function testDispatchWithWrongEventType()
+    {
+        $this->conn->expects($this->never())
+                   ->method('channel');
+
+        $this->dispatcher->dispatch('my.test.event', new \Symfony\Component\EventDispatcher\Event());
+    }
+
+    public function testDispatchWithEventPropagationStopped()
+    {
+        $this->conn->expects($this->never())
+                   ->method('channel');
+
+        $evt = $this->getMockBuilder('CodeNugget\EventDispatcherExtra\Amqp\QueueEvent')
+                    ->disableOriginalConstructor()
+                    ->getMock();
+
+        $evt->expects($this->once())
+            ->method('isPropagationStopped')
+            ->will($this->returnValue(true));
+
+        $this->dispatcher->dispatch('my.test.event', $evt);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testDispatchWithInvalidPublisher()
+    {
+        $this->dispatcher->dispatch('my.failing.event', new QueueEvent(), 'this-will-fail');
+    }
+
+    public function testDispatchWithClosureAsCustomPublisher()
+    {
+        $publisher = function($event, $conn) {
+            $channel = $conn->channel();
+            $channel->close();
+        };
+
+        $channel = $this->getMockBuilder('PhpAmqpLib\Channel\AMQPChannel')
+                        ->disableOriginalConstructor()
+                        ->getMock();
+ 
+        $channel->expects($this->once())
+                ->method('close');
+
+        $this->conn->expects($this->once())
+                   ->method('channel')
+                   ->will($this->returnValue($channel));
+
+        $this->dispatcher->dispatch('my.test.event', new QueueEvent(), $publisher);
+    }
+}
---
+++ test/CodeNugget/EventDispatcherExtra/Amqp/QueueEventTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Test\CodeNugget\EventDispatcherExtra\Amqp;
+
+use CodeNugget\EventDispatcherExtra\Amqp\QueueEvent;
+
+class QueueEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->evt = new QueueEvent();
+    }
+
+    public function tearDown()
+    {
+        unset($this->evt);
+    }
+
+    public function testCreate()
+    {
+        $evt = new QueueEvent('test', array(
+            'exchange' => array(
+                'name'    => 'test-exchange',
+                'type'    => 'fanout',
+                'durable' => true,
+            ),
+            'queue' => array(
+                'name'      => 'test-queue',
+                'exclusive' => true,
+            ),
+        ));
+
+        $this->assertEquals('test-exchange', $evt['exchange']['name']);
+        $this->assertEquals('fanout', $evt['exchange']['type']);
+        $this->assertTrue($evt['exchange']['durable']);
+        $this->assertEquals('test-queue', $evt['queue']['name']);
+        $this->assertTrue($evt['queue']['exclusive']);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testCreateWithInvalidExchangeType()
+    {
+        $evt = new QueueEvent('test', array(
+            'exchange' => array('type' => 'foo')
+        ));
+    }
+
+    public function testSetExchangeName()
+    {
+        $this->evt->setExchangeName('my-test-exchange');
+
+        $this->assertEquals('my-test-exchange', $this->evt['exchange']['name']);
+    }
+
+    public function testSetExchangeType()
+    {
+        $this->evt->setExchangeType('fanout');
+
+        $this->assertEquals('fanout', $this->evt['exchange']['type']);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testSetExchangeTypeWithInvalidType()
+    {
+        $this->evt->setExchangeType('foo');
+    }
+
+    public function testSetQueueName()
+    {
+        $this->evt->setQueueName('my-test-queue');
+
+        $this->assertEquals('my-test-queue', $this->evt['queue']['name']);
+    }
+}
